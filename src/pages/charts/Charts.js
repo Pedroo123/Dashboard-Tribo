6373d888ac3c6d81a37b9d325f68bc1dd8b55902
Pedro Brancalhao <pedroaugusto466@gmail.com>
2020-04-19 00:43:27 -0300

    Comments for chart page

--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -9,6 +9,12 @@ import BarChart from './charts/BarChart';
 import PercentAreaChart from './charts/PercentAreaChart';
 import PieChart from './charts/PieChart';
 
+
+/**
+ * TODO
+ * Logica para receber os dados do mock e renderizalos no Chart
+ */
+
 const data = [
   {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
   {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
